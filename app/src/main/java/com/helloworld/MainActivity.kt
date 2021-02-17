462bf3137aee1d07e957c7f8bb504b61cf5bc23f
Ian Chai <ianchai@mmu.edu.my>
2021-02-17 15:34:18 +0800

    Added toast but the longclick not working yet

--- app/src/main/java/com/helloworld/MainActivity.kt
+++ app/src/main/java/com/helloworld/MainActivity.kt
@@ -6,9 +6,13 @@ import android.os.Parcelable
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity() : AppCompatActivity(){
+
+    private lateinit var textView:TextView
+    private lateinit var editText:EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +20,13 @@ class MainActivity() : AppCompatActivity(){
     }
 
     fun clickIt(view: View) {
-        val textView = findViewById<TextView>(R.id.textView)
-        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
+        textView = findViewById<TextView>(R.id.textView)
+        editText = findViewById<EditText>(R.id.editTextTextPersonName)
+        Toast.makeText(this,"Hello, "+editText.text,Toast.LENGTH_SHORT).show()
         textView.setText("Welcome to programming, "+editText.text)
+    }
+
+    fun longClick(view: View) {
+        Toast.makeText(this,"Long press, "+editText.text,Toast.LENGTH_SHORT).show()
     }
 }
